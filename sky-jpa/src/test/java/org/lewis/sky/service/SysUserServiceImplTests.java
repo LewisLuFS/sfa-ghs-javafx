ddea2e92eb9857f0bf11f311fedee5ba39bea098
lewis007 <lewis.lu.fs@icloud.com>
2016-05-27 11:21:39 +0800

    add logger

--- sky-jpa/src/test/java/org/lewis/sky/service/SysUserServiceImplTests.java
+++ sky-jpa/src/test/java/org/lewis/sky/service/SysUserServiceImplTests.java
@@ -7,6 +7,8 @@ import org.junit.runner.RunWith;
 import org.lewis.sky.MainApp;
 import org.lewis.sky.security.domain.SysUser;
 import org.lewis.sky.security.service.SysUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -15,11 +17,14 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringApplicationConfiguration(MainApp.class)
 public class SysUserServiceImplTests {
 
+	private static final Logger logger = LoggerFactory.getLogger(SysUserServiceImplTests.class);
+
 	@Autowired
 	private SysUserService sysUserSrv;
 
 	@Test
 	public void save() {
+		logger.debug("save()...");
 		long count = this.sysUserSrv.findAll().size();
 
 		SysUser sysUser = new SysUser("431520", "Lewis");
@@ -30,6 +35,7 @@ public class SysUserServiceImplTests {
 
 	@Test
 	public void deleteAll() {
+		logger.debug("deleteAll()...");
 		this.sysUserSrv.deleteAll();
 		assertEquals(0, this.sysUserSrv.findAll().size());
 	}
