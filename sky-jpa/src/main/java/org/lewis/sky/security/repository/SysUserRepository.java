f8d65ab4ed2a37851440a0e7d0396b7e1ea74a25
lewis007 <lewis.lu.fs@icloud.com>
2016-06-07 15:39:13 +0800

    delete sky-jpa

--- sky-jpa/src/main/java/org/lewis/sky/security/repository/SysUserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.lewis.sky.security.repository;
-
-import org.lewis.sky.security.domain.SysUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SysUserRepository extends JpaRepository<SysUser, Long> {
-
-}
